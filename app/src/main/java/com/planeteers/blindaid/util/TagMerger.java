400ea67ad5c51a31658466177e272200642ed8ef
Dean Webb <deanmwebb@gmail.com>
2015-11-10 18:10:54 -0800

    Skeleton MergeTagger

---
+++ app/src/main/java/com/planeteers/blindaid/util/TagMerger.java
@@ -0,0 +1,16 @@
+package com.planeteers.blindaid.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TagMerger {
+
+    public interface Callback {
+        public void onComplete(List<String> tagResults);
+    }
+
+
+    public void mergeTags(List<String> list1, List<String> list2) {
+
+    }
+}
