6eebaa6adc020ddb0497539c1de97afa61782854
Jose <engtoolsdev@gmail.com>
2015-11-10 20:28:20 -0800

    Added PictureTag object

--- app/src/main/java/com/planeteers/blindaid/models/PictureTag.java
+++ app/src/main/java/com/planeteers/blindaid/models/PictureTag.java
@@ -16,21 +16,21 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Imagga {
+public class PictureTag {
 
     public String tagName;
     public Double confidence;
 
-    public Imagga(String tagName, Double confidence){
+    public PictureTag(String tagName, Double confidence){
         this.tagName = tagName;
         this.confidence = confidence;
     }
     public static class ImaggaDeserializer implements JsonDeserializer<List> {
 
         @Override
-        public List<Imagga> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        public List<PictureTag> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
             JsonObject object = json.getAsJsonObject();
-            List<Imagga> imaggaList = new ArrayList<>();
+            List<PictureTag> imaggaList = new ArrayList<>();
 
             if (object.has("results")) {
                 JsonArray array = object.get("results").getAsJsonArray();
@@ -45,7 +45,7 @@ public class Imagga {
                         if (tag.has(Constants.IMAGGA.CONFIDENCE_KEY) && tag.has(Constants.IMAGGA.TAG_KEY)) {
                             Double confidence = tag.get(Constants.IMAGGA.CONFIDENCE_KEY).getAsDouble();
                             String tagName = tag.get(Constants.IMAGGA.TAG_KEY).getAsString();
-                            imaggaList.add(new Imagga(tagName, confidence));
+                            imaggaList.add(new PictureTag(tagName, confidence));
                         }
                     }
                 }
